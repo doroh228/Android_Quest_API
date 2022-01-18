76692c3ec760bb5a61b32d7b12da4fccf7b9496b
sergey <doroh.2001@gmai.com>
2022-01-18 23:34:47 +0300

    добавил миграцию очков

---
+++ app/controllers/role_controller.rb
@@ -0,0 +1,25 @@
+class RoleController < ApplicationController
+  def index
+    @roles = Role.all
+    render json: @roles, status: :ok
+  end
+
+  def create
+    @role = Role.new role_params
+    if @role.save
+      render json: [{status: true, name: @role.name}], status: :ok
+    else
+      render json: false, status: :ok
+    end
+  end
+
+
+
+
+  private
+
+  def role_params
+    params.permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
+  #USERS
   get '/users', to: 'user#index'
   get '/admins', to: 'user#get_admin'
 
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   delete '/users/:id', to: 'user#destroy'
   options '/users/:id', to: 'user#option_r'
 
+  #ROLE
+  get '/roles', to: 'role#index'
+  post '/roles', to: 'role#create'
+
+
+  #QUESTIONS
   post '/questions', to: 'question#create'
 
 end
